6b36c6b01a10b9ba8e43ce0dba11cd9cc0125b0d
geeky-brahma <sahoobrahmananda2004.com>
2024-01-24 22:40:38 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request,  redirect, url_for,jsonify
+import requests
+
+app=Flask(__name__)
+
+# @app.route('/')
+# def welcome():
+#     return "Welcome"
+
+@app.route("/",methods=["GET","POST"])
+def weather():
+    if request.method=="GET":
+        return render_template("index.html")
+    else:
+        city = request.form["city"]
+        url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{city}?key=VKKUQ6APVMMSX79QYT5LMEJFX"
+        response = requests.get(url)
+        if response.status_code == 200:
+            # API call was successful
+            data = response.json()  # Parse the JSON response
+            # print(data)
+        else:
+            # API call failed
+            print(f"Error {response.status_code}: {response.text}")
+        lat=data["latitude"]
+        long=data["longitude"]
+        temp=round((data["days"][0]["temp"]-32)*(5/9),2)
+        flike=round((data["days"][0]["feelslike"]-32)*(5/9),2)  
+        desc=data["description"]
+        alerts=data["alerts"]
+
+        return render_template("result.html", city=city, lat=lat, long=long, temp=temp, flike=flike, desc=desc, alerts=alerts)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
